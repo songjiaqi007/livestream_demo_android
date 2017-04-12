aefb88a18d0da5692661190553af19aba79269ba
songjiaqi007 <609208788@qq.com>
2017-04-12 17:40:37 +0800

    add easeui src

--- easeui/src/com/hyphenate/easeui/controller/EaseUI.java
+++ easeui/src/com/hyphenate/easeui/controller/EaseUI.java
@@ -1,27 +1,25 @@
 package com.hyphenate.easeui.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import com.hyphenate.EMMessageListener;
-import com.hyphenate.chat.EMClient;
-import com.hyphenate.chat.EMMessage;
-import com.hyphenate.chat.EMOptions;
-import com.hyphenate.chat.EMMessage.ChatType;
-import com.hyphenate.easeui.EaseConstant;
-import com.hyphenate.easeui.domain.EaseEmojicon;
-import com.hyphenate.easeui.domain.EaseUser;
-import com.hyphenate.easeui.model.EaseAtMessageHelper;
-import com.hyphenate.easeui.model.EaseNotifier;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.util.Log;
+
+import com.hyphenate.EMMessageListener;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMOptions;
+import com.hyphenate.easeui.domain.EaseEmojicon;
+import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.model.EaseAtMessageHelper;
+import com.hyphenate.easeui.model.EaseNotifier;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public final class EaseUI {
     private static final String TAG = EaseUI.class.getSimpleName();
@@ -30,47 +28,47 @@ public final class EaseUI {
      * the global EaseUI instance
      */
     private static EaseUI instance = null;
-    
+
     /**
      * user profile provider
      */
     private EaseUserProfileProvider userProvider;
-    
+
     private EaseSettingsProvider settingsProvider;
-    
+
     /**
      * application context
      */
     private Context appContext = null;
-    
+
     /**
      * init flag: test if the sdk has been inited before, we don't need to init again
      */
     private boolean sdkInited = false;
-    
+
     /**
      * the notifier
      */
     private EaseNotifier notifier = null;
-    
+
     /**
      * save foreground Activity which registered eventlistener
      */
     private List<Activity> activityList = new ArrayList<Activity>();
-    
+
     public void pushActivity(Activity activity){
         if(!activityList.contains(activity)){
-            activityList.add(0,activity); 
+            activityList.add(0,activity);
         }
     }
-    
+
     public void popActivity(Activity activity){
         activityList.remove(activity);
     }
-    
-    
+
+
     private EaseUI(){}
-    
+
     /**
      * get instance of EaseUI
      * @return
@@ -81,12 +79,12 @@ public final class EaseUI {
         }
         return instance;
     }
-    
+
     /**
      *this function will initialize the SDK and easeUI kit
-     * 
+     *
      * @return boolean true if caller can continue to call SDK related APIs after calling onInit, otherwise false.
-     * 
+     *
      * @param context
      * @param options use default if options is null
      * @return
@@ -96,10 +94,10 @@ public final class EaseUI {
             return true;
         }
         appContext = context;
-        
+
         int pid = android.os.Process.myPid();
         String processAppName = getAppName(pid);
-        
+
         Log.d(TAG, "process app name : " + processAppName);
 
         // if there is application has remote service, application:onCreate() maybe called twice
@@ -114,18 +112,18 @@ public final class EaseUI {
         }else{
             EMClient.getInstance().init(context, options);
         }
-        
+
         initNotifier();
         registerMessageListener();
-        
+
         if(settingsProvider == null){
             settingsProvider = new DefaultSettingsProvider();
         }
-        
+
         sdkInited = true;
         return true;
     }
-    
+
 
     protected EMOptions initChatOptions(){
         Log.d(TAG, "init HuanXin Options");
@@ -137,29 +135,29 @@ public final class EaseUI {
         options.setRequireAck(true);
         // set if need delivery ack
         options.setRequireDeliveryAck(false);
-        
+
         return options;
     }
-    
+
     void initNotifier(){
         notifier = createNotifier();
         notifier.init(appContext);
     }
-    
+
     private void registerMessageListener() {
         EMClient.getInstance().chatManager().addMessageListener(new EMMessageListener() {
-            
+
             @Override
             public void onMessageReceived(List<EMMessage> messages) {
                 EaseAtMessageHelper.get().parseMessages(messages);
             }
             @Override
             public void onMessageChanged(EMMessage message, Object change) {
-                
+
             }
             @Override
             public void onCmdMessageReceived(List<EMMessage> messages) {
-                
+
             }
 
             @Override public void onMessageRead(List<EMMessage> messages) {
@@ -171,27 +169,27 @@ public final class EaseUI {
             }
         });
     }
-    
+
     protected EaseNotifier createNotifier(){
         return new EaseNotifier();
     }
-    
+
     public EaseNotifier getNotifier(){
         return notifier;
     }
-    
+
     public boolean hasForegroundActivies(){
         return activityList.size() != 0;
     }
-    
+
     /**
      * set user profile provider
-     * @param provider
+     * @param userProvider
      */
     public void setUserProfileProvider(EaseUserProfileProvider userProvider){
         this.userProvider = userProvider;
     }
-    
+
     /**
      * get user profile provider
      * @return
@@ -199,16 +197,16 @@ public final class EaseUI {
     public EaseUserProfileProvider getUserProfileProvider(){
         return userProvider;
     }
-    
+
     public void setSettingsProvider(EaseSettingsProvider settingsProvider){
         this.settingsProvider = settingsProvider;
     }
-    
+
     public EaseSettingsProvider getSettingsProvider(){
         return settingsProvider;
     }
-    
-    
+
+
     /**
      * check the application process name if process name is not qualified, then we think it is a service process and we will not init SDK
      * @param pID
@@ -237,7 +235,7 @@ public final class EaseUI {
         }
         return processName;
     }
-    
+
     /**
      * User profile provider
      * @author wei
@@ -250,8 +248,9 @@ public final class EaseUI {
          * @return
          */
         EaseUser getUser(String username);
+        User getAppUser(String username);
     }
-    
+
     /**
      * Emojicon provider
      *
@@ -263,16 +262,16 @@ public final class EaseUI {
          * @return
          */
         EaseEmojicon getEmojiconInfo(String emojiconIdentityCode);
-        
+
         /**
          * get Emojicon map, key is the text of emoji, value is the resource id or local path of emoji icon(can't be URL on internet)
          * @return
          */
         Map<String, Object> getTextEmojiconMapping();
     }
-    
+
     private EaseEmojiconInfoProvider emojiconInfoProvider;
-    
+
     /**
      * Emojicon provider
      * @return
@@ -280,7 +279,7 @@ public final class EaseUI {
     public EaseEmojiconInfoProvider getEmojiconInfoProvider(){
         return emojiconInfoProvider;
     }
-    
+
     /**
      * set Emojicon provider
      * @param emojiconInfoProvider
@@ -288,7 +287,7 @@ public final class EaseUI {
     public void setEmojiconInfoProvider(EaseEmojiconInfoProvider emojiconInfoProvider){
         this.emojiconInfoProvider = emojiconInfoProvider;
     }
-    
+
     /**
      * new message options provider
      *
@@ -299,7 +298,7 @@ public final class EaseUI {
         boolean isMsgVibrateAllowed(EMMessage message);
         boolean isSpeakerOpened();
     }
-    
+
     /**
      * default settings provider
      *
@@ -325,9 +324,9 @@ public final class EaseUI {
         @Override
         public boolean isSpeakerOpened() {
             return true;
-        } 
+        }
     }
-    
+
     public Context getContext(){
         return appContext;
     }
--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -10,15 +10,28 @@ import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
-    
+
     static EaseUserProfileProvider userProvider;
-    
+
     static {
         userProvider = EaseUI.getInstance().getUserProfileProvider();
     }
-    
+
+    /**
+     * get User according username
+     * @param username
+     * @return
+     */
+    public static User getAppUserInfo(String username){
+        if(userProvider != null)
+            return userProvider.getAppUser(username);
+
+        return null;
+    }
+
     /**
      * get EaseUser according username
      * @param username
@@ -27,41 +40,96 @@ public class EaseUserUtils {
     public static EaseUser getUserInfo(String username){
         if(userProvider != null)
             return userProvider.getUser(username);
-        
+
         return null;
     }
-    
+
     /**
      * set user avatar
      * @param username
      */
     public static void setUserAvatar(Context context, String username, ImageView imageView){
-    	EaseUser user = getUserInfo(username);
-        if(user != null && user.getAvatar() != null){
-            try {
-                int avatarResId = Integer.parseInt(user.getAvatar());
-                Glide.with(context).load(avatarResId).into(imageView);
-            } catch (Exception e) {
-                //use default avatar
-                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
-            }
+        EaseUser user = getUserInfo(username);
+        if(user != null){
+            setAvatar(context,user.getAvatar(),imageView);
         }else{
-            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
         }
     }
-    
+
     /**
      * set user's nickname
      */
     public static void setUserNick(String username,TextView textView){
         if(textView != null){
-        	EaseUser user = getUserInfo(username);
-        	if(user != null && user.getNick() != null){
-        		textView.setText(user.getNick());
-        	}else{
-        		textView.setText(username);
-        	}
+            EaseUser user = getUserInfo(username);
+            if(user != null && user.getNick() != null){
+                textView.setText(user.getNick());
+            }else{
+                textView.setText(username);
+            }
         }
     }
-    
+
+    /**
+     * set user avatar
+     * @param username
+     */
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView){
+        User user = getAppUserInfo(username);
+        setAppUserAvatar(context,user,imageView);
+
+    }
+
+    /**
+     * set user avatar
+     * @param user
+     */
+    public static void setAppUserAvatar(Context context, User user, ImageView imageView){
+        if(user != null){
+            setAvatar(context,user.getAvatar(),imageView);
+        }else{
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+
+    }
+
+    public static void setAvatar(Context context,String avatarPath, ImageView imageView){
+        if(avatarPath != null){
+            try {
+                int avatarResId = Integer.parseInt(avatarPath);
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(avatarPath).diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .placeholder(R.drawable.default_hd_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+    }
+
+
+    /**
+     * set user's nickname
+     */
+    public static void setAppUserNick(String username,TextView textView){
+        if(textView != null){
+            User user = getAppUserInfo(username);
+            setAppUserNick(user,textView);
+        }
+    }
+
+    /**
+     * set user's nickname
+     */
+    public static void setAppUserNick(User user,TextView textView){
+        if(textView != null && user !=null){
+            if(user.getMUserNick() != null){
+                textView.setText(user.getMUserNick());
+            }else{
+                textView.setText(user.getMUserName());
+            }
+        }
+    }
 }

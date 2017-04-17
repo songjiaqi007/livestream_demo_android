e3f265fcf253501ba70079f5e6dc76950c1ef4bb
songjiaqi007 <609208788@qq.com>
2017-04-17 08:57:35 +0800

    在UserDao中调用DBManager的方法

--- app/src/main/java/cn/ucai/live/db/GiftsDao.java
+++ app/src/main/java/cn/ucai/live/db/GiftsDao.java
@@ -1,5 +1,11 @@
 package cn.ucai.live.data.local;
 
+import java.util.List;
+import java.util.Map;
+
+import cn.ucai.live.data.DBManager;
+import cn.ucai.live.data.model.Gift;
+
 /**
  * Created by clawpo on 2017/4/14.
  */
@@ -10,4 +16,26 @@ public class GiftDao {
     public static final String GIFT_COLUMN_NAME = "m_gift_name";
     public static final String GIFT_COLUMN_URL = "m_gift_url";
     public static final String GIFT_COLUMN_PRICE = "m_gift_price";
+
+    public GiftDao() {
+    }
+
+    /**
+     * save gift list
+     *
+     * @param giftList
+     */
+    public void saveAppGiftList(List<Gift> giftList) {
+        DBManager.getInstance().saveAppGiftList(giftList);
+    }
+
+    /**
+     * get gift list
+     *
+     * @return
+     */
+    public Map<Integer, Gift> getAppGiftList() {
+
+        return DBManager.getInstance().getAppGiftList();
+    }
 }
